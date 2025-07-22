f8479e669f1fc02e8bea1807ac2e509537720b20
Alvaro Romero <alvaro_romero35@hotmail.com>
2025-07-22 16:52:20 -0600

    chore: cambio de carpeta de componentes para compatibilidad con Windows

---
+++ resources/js/Components/ui/alert/Alert.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+import { cn } from '@/lib/utils'
+import { type AlertVariants, alertVariants } from './index'
+
+const props = defineProps<{
+  class?: HTMLAttributes['class']
+  variant?: AlertVariants['variant']
+}>()
+</script>
+
+<template>
+  <div :class="cn(alertVariants({ variant }), props.class)" role="alert">
+    <slot />
+  </div>
+</template>
