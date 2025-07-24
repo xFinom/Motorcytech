aff75c5803a7af27147203749c5d69bc2041e756
alvaro <alvaro_romero35@hotmail.com>
2025-07-23 21:16:23 -0700

    chore: cambios para laravel herd

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,35 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.ts',
-            ssr: 'resources/js/ssr.ts',
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        plugins: [
+            laravel({
+                input: 'resources/js/app.ts',
+                ssr: 'resources/js/ssr.ts',
+                refresh: true,
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
                 },
+            }),
+        ],
+        server: {
+            host: '0.0.0.0',
+            port: 5173,
+            strictPort: true,
+            cors: true,
+            hmr: {
+                host: env.VITE_APP_HOST,
+                protocol: env.VITE_APP_PROTOCOL,
             },
-        }),
-    ],
-    server: {
-        host: '0.0.0.0', // útil para Docker
-        port: 5173,
-        strictPort: true,
-        cors: true,
-        hmr: {
-            host: 'localhost', // o tu IP si estás en red/Docker
         },
-    },
+    };
 });
