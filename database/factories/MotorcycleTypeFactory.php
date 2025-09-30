f57edbad219219cc847737ea7d2de58552d97605
romero <alvaro_romero35@hotmail.com>
2025-09-30 08:57:58 -0600

    upd: seeders without faker methods

--- database/factories/MotorcycleTypeFactory.php
+++ database/factories/MotorcycleTypeFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Brand;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\MotorcycleType>
@@ -18,7 +19,7 @@ class MotorcycleTypeFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->randomElement(['Deportiva', 'Enduro', 'Naked', 'Scooter']),
+            'name' => Arr::random(['Deportiva', 'Enduro', 'Naked', 'Scooter']),
             'brand_id' => Brand::query()->inRandomOrder()->value('id'),
         ];
     }
--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -17,12 +17,12 @@ class ServiceSeeder extends Seeder
     {
         DB::transaction(function () {
             DB::table('services')->insert([
-                ['id' => 1, 'name' => 'Mantenimiento Preventivo', 'description' => fake()->paragraph()],
-                ['id' => 2, 'name' => 'Reparación', 'description' => fake()->paragraph()],
-                ['id' => 3, 'name' => 'Enderezado de Pipas', 'description' => fake()->paragraph()],
-                ['id' => 4, 'name' => 'Escaneo', 'description' => fake()->paragraph()],
-                ['id' => 5, 'name' => 'Carburación y Fuel Injection', 'description' => fake()->paragraph()],
-                ['id' => 6, 'name' => 'Revisión General', 'description' => fake()->paragraph()],
+                ['id' => 1, 'name' => 'Mantenimiento Preventivo', 'description' => 'Descripcion de prueba'],
+                ['id' => 2, 'name' => 'Reparación', 'description' => 'Descripcion de prueba'],
+                ['id' => 3, 'name' => 'Enderezado de Pipas', 'description' => 'Descripcion de prueba'],
+                ['id' => 4, 'name' => 'Escaneo', 'description' => 'Descripcion de prueba'],
+                ['id' => 5, 'name' => 'Carburación y Fuel Injection', 'description' => 'Descripcion de prueba'],
+                ['id' => 6, 'name' => 'Revisión General', 'description' => 'Descripcion de prueba'],
             ]);
         });
     }
